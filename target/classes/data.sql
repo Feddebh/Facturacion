e3733cd308a076ddb661033e075ea200b58dc7f2
feddebh <bluisfederico@gmail.com>
2023-04-05 01:42:54 -0300

    Created Jar

---
+++ target/classes/data.sql
@@ -0,0 +1,23 @@
+INSERT INTO clients (name, lastname, doc_number)
+VALUES ('Federico', 'Bohle', 36675955);
+
+INSERT INTO clients (name, lastname, doc_number)
+VALUES ('Gustavo', 'Martinez', 21567353);
+
+INSERT INTO clients (name, lastname, doc_number)
+VALUES ('Carolina', 'Perez', 23456586);
+
+INSERT INTO clients (name, lastname, doc_number)
+VALUES ('Gabriel', 'Moreyra', 42566876);
+
+INSERT INTO products (code, description, price, stock)
+VALUES ('123', 'Coca-Cola 3l', 600.00, 25);
+
+INSERT INTO products (code, description, price, stock)
+VALUES ('153', 'Mani tostado pepsico 50grs', 455.00, 43);
+
+INSERT INTO products (code, description, price, stock)
+VALUES ('1123', 'Desodorante Poett 360 cm3', 700.00, 78);
+
+INSERT INTO products (code, description, price, stock)
+VALUES ('567', 'Dog chow 3kg', 1900.00, 25);
---
+++ target/classes/schema.sql
@@ -0,0 +1,41 @@
+CREATE TABLE IF NOT EXISTS clients
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(75) NOT NULL,
+    lastName VARCHAR(75) NOT NULL,
+    docNumber VARCHAR(11) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+
+CREATE TABLE IF NOT EXISTS invoice
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    created_at DATETIME NOT NULL,
+    total DECIMAL(10,2) NOT NULL,
+    client_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (client_id) REFERENCES clients(id)
+);
+
+CREATE TABLE IF NOT EXISTS products
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    description VARCHAR(150) NOT NULL,
+    code VARCHAR(50) NOT NULL,
+    stock INT NOT NULL,
+    price DECIMAL(10,2),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS invoice_details
+(
+    invoice_detail_id INT NOT NULL AUTO_INCREMENT,
+    invoice_id INT,
+    total DECIMAL(10,2) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    product_id INT NOT NULL,
+    PRIMARY KEY (invoice_detail_id),
+    FOREIGN KEY (product_id) REFERENCES products(id)
+);
+
